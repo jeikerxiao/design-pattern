5ef5b5698ed0f0d050a0cbce2c661cc9666138b0
jeikerxiao <jeiker@126.com>
2018-03-30 16:29:33 +0800

    optimize log output
    optimize log output

--- design-pattern/src/main/java/com/jeiker/designpattern/behavior/classstatus/MementoDemo.java
+++ design-pattern/src/main/java/com/jeiker/designpattern/behavior/classstatus/MementoDemo.java
@@ -90,18 +90,18 @@ public class MementoDemo {
 	public static void main(String[] args) {
 
 		// 创建原始类
-		Original origi = new Original("egg");
+		Original origi = new Original("鸡蛋");
 
 		// 创建备忘录
 		Storage storage = new Storage(origi.createMemento());
 
 		// 修改原始类的状态
-		System.out.println("初始化状态为: " + origi.getValue());
-		origi.setValue("niu");
-		System.out.println("修改后的状态为: " + origi.getValue());
+		System.out.println("1.初始化的状态为: " + origi.getValue());
+		origi.setValue("小鸡");
+		System.out.println("2.修改后的状态为: " + origi.getValue());
 
 		// 回复原始类的状态
 		origi.restoreMemento(storage.getMemento());
-		System.out.println("恢复后的状态为: " + origi.getValue());
+		System.out.println("3.恢复后的状态为: " + origi.getValue());
 	}
 }
